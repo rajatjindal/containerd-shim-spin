9e6f3b7b676f455e234936a35ea8d41e535f7390
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-20 16:14:56 +0530

    try increasing timeout
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/workloads-delete.sh
+++ scripts/workloads-delete.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 ## test that the workload pods can be terminated
-kubectl delete pod -l app=wasm-spin --timeout 10s
-kubectl delete pod -l app=spin-keyvalue --timeout 10s
-kubectl delete pod -l app=spin-outbound-redis --timeout 10s
+kubectl delete pod -l app=wasm-spin --timeout 20s
+kubectl delete pod -l app=spin-keyvalue --timeout 20s
+kubectl delete pod -l app=spin-outbound-redis --timeout 20s
 
