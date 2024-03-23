b82d2d888b806841aa310d4c344f10f97d5e1708
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-23 22:07:01 +0530

    fixup
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/workloads-delete.sh
+++ scripts/workloads-delete.sh
@@ -7,11 +7,14 @@ set -euo pipefail
 NODE=`kubectl get pod $(kubectl get pod -l app=wasm-spin --no-headers | awk '{print $1}') --no-headers -o custom-columns=":spec.nodeName"`
 echo "node is $NODE"
 
-kubectl delete pod -l app=wasm-spin --timeout 50s || true
-kubectl describe pod -l app=wasm-spin
-
 CID=`docker ps | grep "$NODE" | awk '{print $1}'`
 echo "cid is $CID"
+
+
+kubectl delete pod -l app=wasm-spin --timeout 20s || true
+docker exec $CID ps -ef
+
+kubectl describe pod -l app=wasm-spin
 
 docker exec $CID ps -ef
 docker exec $CID cat /var/lib/rancher/k3s/agent/containerd/containerd.log
