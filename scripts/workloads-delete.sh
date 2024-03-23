652a07f2068767b2e169a1bd738d19b87ed810eb
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-23 19:16:51 +0530

    make it even faster
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/workloads-delete.sh
+++ scripts/workloads-delete.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 ## test that the workload pods can be terminated
-kubectl delete pod -l app=wasm-spin --timeout 20s
-kubectl delete pod -l app=spin-keyvalue --timeout 20s
-kubectl delete pod -l app=spin-outbound-redis --timeout 20s
+kubectl delete pod -l app=wasm-spin --timeout 20s || true
+kubectl describe pod -l app=wasm-spin
 
