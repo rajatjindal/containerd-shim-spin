624ea5b165af71dc14d339ffc3464a306dfcf7ae
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-23 19:29:51 +0530

    check containerd logs
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/workloads-delete.sh
+++ scripts/workloads-delete.sh
@@ -9,7 +9,7 @@ kubectl describe pod -l app=wasm-spin
 NODE=`kubectl get pod $(kubectl get pod -l app=wasm-spin --no-headers | grep -i Terminating | awk '{print $1}') --no-headers -o custom-columns=":spec.nodeName"`
 echo "node is $NODE"
 
-cid=`docker ps | grep "$NODE" | awk '{print $1}'`
+CID=`docker ps | grep "$NODE" | awk '{print $1}'`
 echo "cid is $CID"
 
-docker exec -it $cid tail -f /var/lib/rancher/k3s/agent/containerd/containerd.log
+docker exec -it $CID tail -f /var/lib/rancher/k3s/agent/containerd/containerd.log
