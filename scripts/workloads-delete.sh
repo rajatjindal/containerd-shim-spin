be8da7a4594749d7e635455a44211699a1c5ee3b
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-23 21:35:32 +0530

    fixup
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/workloads-delete.sh
+++ scripts/workloads-delete.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 
 ## test that the workload pods can be terminated
+NODE=`kubectl get pod $(kubectl get pod -l app=wasm-spin --no-headers | awk '{print $1}') --no-headers -o custom-columns=":spec.nodeName"`
+echo "node is $NODE"
+
 kubectl delete pod -l app=wasm-spin --timeout 50s || true
 kubectl describe pod -l app=wasm-spin
-
-NODE=`kubectl get pod $(kubectl get pod -l app=wasm-spin --no-headers | grep -i Terminating | awk '{print $1}') --no-headers -o custom-columns=":spec.nodeName"`
-echo "node is $NODE"
 
 CID=`docker ps | grep "$NODE" | awk '{print $1}'`
 echo "cid is $CID"
