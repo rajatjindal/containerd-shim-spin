881fcb9a1eec64ce28924fbaae1f2b2077b0387e
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-25 22:27:32 +0530

    add top command to get a feel of resources available
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/workloads-delete.sh
+++ scripts/workloads-delete.sh
@@ -2,16 +2,23 @@
 
 set -euo pipefail
 
+top
 
 ## test that the workload pods can be terminated
 NODE=`kubectl get pod $(kubectl get pod -l app=wasm-spin --no-headers | awk '{print $1}') --no-headers -o custom-columns=":spec.nodeName"`
 echo "node is $NODE"
 
+top 
+
 CID=`docker ps | grep "$NODE" | awk '{print $1}'`
 echo "cid is $CID"
 
+top
 
 kubectl delete pod -l app=wasm-spin --timeout 20s || true
+
+top
+
 docker exec $CID ps -ef
 
 kubectl describe pod -l app=wasm-spin
