33f55c2a37ad4292d457bcc2844896f3c78c431d
Rajat Jindal <rajatjindal83@gmail.com>
2024-04-19 14:16:32 +0530

    update wasm path in test apps and push to local registry
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/up.sh
+++ scripts/up.sh
@@ -12,18 +12,25 @@ IMAGES=("spin-hello-world" "spin-keyvalue" "spin-outbound-redis" "spin-multi-tri
 # build the Docker image for the k3d cluster
 docker build -t k3d-shim-test "$dockerfile_path"
 
-k3d cluster create "$cluster_name" --image k3d-shim-test --api-port 6551 -p '8082:80@loadbalancer' --agents 2
+k3d cluster create "$cluster_name" \
+  --image k3d-shim-test --api-port 6551 -p '8082:80@loadbalancer' --agents 2 \
+  --registry-create test-registry:0.0.0.0:5000
 
 kubectl wait --for=condition=ready node --all --timeout=120s
 
 # Iterate through the Docker images and build them
 for i in "${!DOCKER_IMAGES[@]}"; do
-  docker buildx build -t "${IMAGES[$i]}:latest" "./images/${DOCKER_IMAGES[$i]}" --load
-  mkdir -p "${OUT_DIRS[$i]}"
-  docker save -o "${OUT_DIRS[$i]}/img.tar" "${IMAGES[$i]}:latest"
-  k3d image import "${OUT_DIRS[$i]}/img.tar" -c "$cluster_name"
+  ## we can push images to localhost:5000 registry, and pull will still work using registry name
+  docker buildx build -t "localhost:5000/${IMAGES[$i]}:latest" "./images/${DOCKER_IMAGES[$i]}"
+  docker push "localhost:5000/${IMAGES[$i]}:latest"
 done
 
 sleep 5
+
+## wait for middleware crd. without this intermittent failures occur on local setup
+for run in {1..60}; do
+  kubectl get crd/middlewares.traefik.containo.us && break || echo "waiting for traefik crd. try #$run of 60"
+  sleep 1
+done
 
 echo ">>> cluster is ready"
