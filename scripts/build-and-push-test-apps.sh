a9f562a8c64c276eb527648cb02d67a81739d8b9
Rajat Jindal <rajatjindal83@gmail.com>
2024-04-22 15:23:10 +0530

    do spin registry push of apps as well
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- scripts/build-and-push-test-apps.sh
+++ scripts/build-and-push-test-apps.sh
@@ -7,7 +7,13 @@ IMAGES=("spin-hello-world" "spin-keyvalue" "spin-outbound-redis" "spin-multi-tri
 
 # Iterate through the Docker images and build them
 for i in "${!DOCKER_IMAGES[@]}"; do
+  echo "building and pushing docker-build and spin-registry-push version of ${DOCKER_IMAGES[$i]}"
+
   ## we can push images to localhost:5000 registry, and pull will still work using registry name
   docker buildx build -t "localhost:5000/${IMAGES[$i]}:latest" "./images/${DOCKER_IMAGES[$i]}"
   docker push "localhost:5000/${IMAGES[$i]}:latest"
+
+  ## also do spin builds and spin registry push
+  spin build -f "./images/${DOCKER_IMAGES[$i]}/spin.toml"
+  spin registry push "localhost:5000/spin-registry-push/${IMAGES[$i]}:latest" -f "./images/${DOCKER_IMAGES[$i]}/spin.toml" -k
 done
